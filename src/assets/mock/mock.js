fa57a830a5b28f198e1c886d10ccb45127157a65
THAWIWAT05 <thawiwat05254905@gmail.com>
2025-11-09 20:31:45 +0700

    Add files via upload

---
+++ src/assets/mock/mock.js
@@ -0,0 +1,48 @@
+import floating1 from "../images/floating/1.png";
+import floating2 from "../images/floating/2.png";
+import image1 from "../images/1.png";
+import image2 from "../images/2.png";
+import image3 from "../images/3.png";
+import image4 from "../images/4.png";
+import image5 from "../images/5.png";
+import gift1 from "../images/gifts/1.png";
+import gift2 from "../images/gifts/2png";
+import star from "../star.svg";
+
+export const _floatingImageFirst = floating1;
+export const _floatingImageSecond = floating2;
+
+export const _albums = [image1, image2, image3, image4, image5];
+
+export const giftAlbums = [gift1, gift2];
+
+export const _stars = [
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "12",
+    },
+    {
+        src: star,
+        color: "#ffccd7",
+        size: "4",
+    },
+];
+
+export const _messages = [
+    "็Happy Birthday to you นะคับ ขอให้พี่มีความสุขมากๆ สุขภาพแข็งแรง สมหวังในทุกสิ่งที่ตั้งใจไว้เลยนะนะ",
+    "ขอให้มีแต่ความสุขในทุกๆๆวัน เจอแต่เรื่องดีๆๆเข้ามาในชีวิตนะคับ 😊 🌸",
+];
+
+export const _birthdayMessages = [
+    "สุขสันต์วันเกิดค้าบบบบบบ 🎉🎂🎁🎈",
+    "วันนี้ถือว่าเป็นวันดีสำหรับพี่เลย ยิ้มเยอะๆนะครับ",
+    "ก็ไม่รู้จะอวยพรยังไง แต่ก็ ขอให้พี่มีความสุขมากๆๆนะ สาวขนส่ง",
+    "สุดท้ายนี้ สุขสันต์วันเกิดนะค้าบบบบ 🎉🎂🎁🎈",
+];
+
+// เอาแค่ code ของคลิปมาใส่ หลัง https://www.youtube.com/embed/ เช่น https://youtube.com/shorts/equ0y9yuLYk จะใช้แค่ equ0y9yuLYk
+// https://www.youtube.com/embed/equ0y9yuLYk
+export const _youtubeUrl = "https://www.youtube.com/embed/gDTDFz0PL78?si=_NWbXqFKt1f8e7p6";
+
+export const _otp = "fai";
